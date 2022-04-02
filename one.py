4c34bef2ebbe6b36e8342526590c63434faf7f4f
myWorkshop123 <kartikkumar868@gmail.com>
2022-04-02 22:27:20 +0530

    first commit

---
+++ one.py
@@ -0,0 +1,14 @@
+import argparse
+# Create a object of the ArgumentParser class 
+parser = argparse.ArgumentParser()
+
+# Specift the argument that you want to add 
+parser.add_argument("echo")
+
+# parse_args function will recieve the arguements from the cli and return a tuple 
+args = parser.parse_args()
+
+# args: Namespace(argument:value)
+print(args.echo)
+
+
