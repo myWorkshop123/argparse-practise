552c6238cbe49f6a252ef475fc687789d3e4047e
myWorkshop123 <kartikkumar868@gmail.com>
2022-04-03 13:27:39 +0530

    formatter class added

--- one.py
+++ one.py
@@ -1,12 +1,16 @@
 import argparse
 # Create a object of the ArgumentParser class 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="Practise of argparse module",formatter_class=argparse.RawDescriptionHelpFormatter)
+# for formatter_class=https://docs.python.org/3/library/argparse.html#formatter-class
 
 # Specift the argument that you want to add 
 parser.add_argument("square",help="Display the square of the number",type=int)
+parser.add_argument("--verbose",help="Increase output verbosity ")
 
 # parse_args function will recieve the arguements from the cli and return a tuple 
 args = parser.parse_args()
+if args.verbose:
+    print("verbosity turned on")
 
 print(args.square ** 2)
 
---
+++ second.py
@@ -0,0 +1,11 @@
+import sys
+
+
+def main(argv):
+    # print(argv)
+    """ Your Best Friend """
+    print(__doc__)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
